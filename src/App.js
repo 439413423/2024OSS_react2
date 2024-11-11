5818ae0ddc3291c75dc5d8d1547c30ee9c33e564
439413423 <22100582@handong.ac.kr>
2024-11-11 21:20:47 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import List from './pages/List';
+import List from './components/pages/List'; 
 
 function App() {
   return (
--- src/components/pages/List.js
+++ src/components/pages/List.js
@@ -1,4 +1,3 @@
-// src/pages/List.js
 import React, { useState, useEffect } from 'react';
 import CreateUser from '../User/CreateUser';
 import EditUser from '../User/EditUser';
@@ -11,37 +10,33 @@ const List = () => {
 
   const apiUrl = "https://672819d3270bd0b975545f98.mockapi.io/api/vi/users";
 
-  // 데이터 가져오기 함수
   const fetchData = async () => {
     const response = await fetch(apiUrl);
     const result = await response.json();
     setData(result);
   };
 
-  // 사용자 추가 모달 열기
+
   const openCreateModal = () => {
     setCreateOpen(true);
   };
 
-  // 사용자 수정 모달 열기
   const openEditModal = (user) => {
     setSelectedUser(user);
     setEditOpen(true);
   };
 
-  // 모달 닫기
   const closeModal = () => {
     setCreateOpen(false);
     setEditOpen(false);
     setSelectedUser(null);
   };
 
-  // 삭제 기능 추가
   const deleteUser = async (userId) => {
     const confirmDelete = window.confirm("정말 삭제하시겠습니까?");
     if (confirmDelete) {
       await fetch(`${apiUrl}/${userId}`, { method: 'DELETE' });
-      fetchData(); // 삭제 후 데이터 목록 다시 불러오기
+      fetchData(); 
     }
   };
 
