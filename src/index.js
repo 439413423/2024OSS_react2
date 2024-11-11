41372a5b6431db46249e2fcd177220d87c60952b
439413423 <22100582@handong.ac.kr>
2024-11-11 15:13:08 +0900

    .

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const myElement = <h1>I Love JSX!</h1>; // JSX
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
